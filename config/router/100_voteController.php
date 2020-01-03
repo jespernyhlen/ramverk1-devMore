10251a5112c0d70012bdb070dfaa59b97e0a85d0
Jesper Nyhlen <jeppe_nyhlen@hotmail.com>
2020-01-03 17:46:09 +0800

    Adding routerfiles for controllers

---
+++ config/router/100_voteController.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Routes for controller.
+ */
+return [
+    "routes" => [
+        [
+            "info" => "Controller for votes.",
+            "mount" => "vote",
+            "handler" => "\Jen\Vote\VoteController",
+        ],
+    ]
+];
---
+++ config/router/500_home.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Flat file controller for reading markdown files from content/.
+ */
+// return [
+//     "routes" => [
+//         [
+//             "info" => "Flat file content controller.",
+//             "mount" => null,
+//             "handler" => "\Anax\Content\FileBasedContentController",
+//         ],
+//     ]
+// ];
+
+return [
+    "routes" => [
+        [
+            "info" => "Flat file content controller.",
+            "mount" => "",
+            "handler" => "\Jen\Home\HomeController",
+        ],
+    ]
+];
+
